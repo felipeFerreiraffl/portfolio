545a45dc3c3f57eceb5f8556d1cb8f7476a13c93
felipeFerreiraffl <felipado2x0@gmail.com>
2025-04-30 22:20:37 -0300

    feat: new react/next configuration

---
+++ src/services/consts/drawings.js
@@ -0,0 +1,11 @@
+const drawings = [
+  {
+    id: 1,
+    title: "",
+    src: "",
+    width: "",
+    height: "",
+  },
+];
+
+export default drawings;
---
+++ src/services/consts/players.js
@@ -0,0 +1,10 @@
+const players = [
+  {
+    id: 1,
+    name: "",
+    src: "",
+    link: "",
+  },
+];
+
+export default players;
