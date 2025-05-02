83432022cb82248742ad6753bbf993b76304bc42
felipeFerreiraffl <felipado2x0@gmail.com>
2025-05-02 11:10:54 -0300

    docs: added variables for colors, fonts and other (spacing, border, opacity, etc)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import themes from "@/hooks/themes/themes";
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div
+      className={themes.fonts.family.ox}
+      style={{ color: themes.colors.neutral.nt03, fontWeight: themes.fonts.weight.bold, fontSize: themes.fonts.size.h1 }}
+    >
+      Home
+    </div>
+  );
 }
---
+++ src/hooks/themes/themes.js
@@ -0,0 +1,7 @@
+import { fonts } from "./fonts/fonts";
+import { colors } from "./colors/colors";
+import { others } from "./other/other";
+
+const themes = { colors, fonts, others };
+
+export default themes;
