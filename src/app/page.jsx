6fd8af0cade7f7bc340099f5f55bda58615e51b9
felipeFerreiraffl <felipado2x0@gmail.com>
2025-05-03 15:39:28 -0300

    feat: new icons added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,12 @@
-import themes from "@/hooks/themes/themes";
 import React from "react";
+import * as G from "../hooks/global/global";
+import * as S from "./styles";
+import themes from "@/hooks/themes/themes";
 
 export default function Home() {
   return (
-    <div
-      className={themes.fonts.family.ox}
-      style={{ color: themes.colors.neutral.nt03, fontWeight: themes.fonts.weight.bold, fontSize: themes.fonts.size.h1 }}
-    >
-      Home
-    </div>
+    <G.Container>
+      <S.Title className={themes.fonts.family.spGk  }>Home</S.Title>
+    </G.Container>
   );
 }
---
+++ src/app/styles.js
@@ -0,0 +1,9 @@
+"use client";
+
+import styled from "styled-components";
+import * as G from "../hooks/global/global";
+import themes from "@/hooks/themes/themes";
+
+export const Title = styled(G.Body)`
+  color: white;
+`;
