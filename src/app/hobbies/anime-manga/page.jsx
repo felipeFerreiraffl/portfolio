4d0c76cadbf80232542dbc0455b534dd60505c0c
felipeFerreiraffl <felipado2x0@gmail.com>
2025-05-03 21:06:44 -0300

    delete: deleted all

--- src/app/hobbies/anime-manga/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AnimeManga() {
-  return (
-    <div>page</div>
-  )
-}
--- src/hooks/themes/themes.js
+++
@@ -1,7 +0,0 @@
-import { colors } from "./colors/colors";
-import { fonts } from "./fonts/fonts";
-import { others } from "./other/other";
-
-const themes = { colors, fonts, others };
-
-export default themes;
