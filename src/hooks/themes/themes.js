f196f2ebf733eef781779a838c48005be974da53
felipeFerreiraffl <felipado2x0@gmail.com>
2025-05-07 18:29:36 -0300

    delete: deleted all

--- src/hooks/themes/themes.js
+++
@@ -1,7 +0,0 @@
-import { fonts } from "./fonts/fonts";
-import { colors } from "./colors/colors";
-import { others } from "./other/other";
-
-const themes = { colors, fonts, others };
-
-export default themes;
