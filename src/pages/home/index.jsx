84ac823f2401d91ef48491f1ece687c230a02acf
felipeFerreiraffl <felipado2x0@gmail.com>
2025-05-22 19:47:50 -0300

    docs: new section for description about me

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,6 +4,8 @@ import styles from "./style.module.css";
 import logo from "../../services/constants/svgs/logo";
 import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
+import { FaIdCard } from "react-icons/fa6";
+import Divisor from "../../ui/components/Divisor";
 
 export default function Home() {
   const { t } = useTranslation("home", { useSuspense: true });
@@ -21,7 +23,7 @@ export default function Home() {
           <motion.img
             className={styles.logo}
             src={logo.altLogo2}
-            alt=""
+            alt={styles}
             loading="lazy"
             initial={{ y: -50, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
@@ -38,6 +40,50 @@ export default function Home() {
           </motion.div>
         </div>
       </header>
+
+      <motion.section
+        className={styles["about-me"]}
+        initial={{ x: 100, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1.2, ease: "easeInOut" }}
+      >
+        <h2 className={styles["sec-ttl"]}>{t("sections.about-me.title")}</h2>
+
+        <div className={styles["abt-cont"]}>
+          <FaIdCard className={styles["abt-icn"]} />
+
+          <div className={styles["abt-txt-ctn"]}>
+            <div className={styles["abt-info"]}>
+              <h5 className={`${styles["quint-ttl"]} ${styles["bd-ttl"]}`}>
+                {t("sections.about-me.name")}
+              </h5>
+              <p className={styles.body}>Felipe Ferreira Lima</p>
+            </div>
+            <div className={styles["abt-info"]}>
+              <h5 className={`${styles["quint-ttl"]} ${styles["bd-ttl"]}`}>
+                {t("sections.about-me.birthday")}
+              </h5>
+              <p className={styles.body}>14/07/2004</p>
+            </div>
+            <div className={styles["abt-info"]}>
+              <h5 className={`${styles["quint-ttl"]} ${styles["bd-ttl"]}`}>
+                {t("sections.about-me.nationality.title")}
+              </h5>
+              <p className={styles.body}>
+                {t("sections.about-me.nationality.desc")}
+              </p>
+            </div>
+            <div className={styles["abt-info"]}>
+              <h5 className={`${styles["quint-ttl"]} ${styles["bd-ttl"]}`}>
+                {t("sections.about-me.goal.title")}
+              </h5>
+              <p className={styles.body}>{t("sections.about-me.goal.desc")}</p>
+            </div>
+          </div>
+        </div>
+      </motion.section>
+
+      <Divisor marginTop={128} color={"var(--main-01)"} />
     </div>
   );
 }
---
+++ src/services/api/other.js
@@ -0,0 +1,9 @@
+const API_URL = "https://portfolio-backend-ex33.onrender.com/api";
+
+export const getProjects = () => {};
+
+export const getPlayers = () => {};
+
+export const getPositions = () => {};
+
+export const getDrawings = () => {};
