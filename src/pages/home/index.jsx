f4d2ed40ca3700122e8982126120c001caa44c41
felipeFerreiraffl <felipado2x0@gmail.com>
2025-05-22 18:28:25 -0300

    docs: translation on home intro

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,8 +2,12 @@ import { Helmet } from "react-helmet";
 import Header from "../../ui/components/Header";
 import styles from "./style.module.css";
 import logo from "../../services/constants/svgs/logo";
+import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
 export default function Home() {
+  const { t } = useTranslation("home", { useSuspense: true });
+
   return (
     <div className={styles.ctn}>
       <Helmet>
@@ -14,20 +18,24 @@ export default function Home() {
         <Header />
 
         <div className={styles.intro}>
-          <img
+          <motion.img
             className={styles.logo}
             src={logo.altLogo2}
             alt=""
             loading="lazy"
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1.2, delay: 0.3, ease: "easeInOut" }}
           />
-          <div className={styles["txt-ctn"]}>
-            <h1 className={styles.ttl}>Bem vindo ao meu portfólio!</h1>
-            <p className={styles.subttl}>
-              Portfólio profissional com o objetivo de mostrar projetos, cursos
-              e habilidades do usuário deste site, como também conteúdo extra,
-              como hobbies
-            </p>
-          </div>
+          <motion.div
+            className={styles["txt-ctn"]}
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1.2, delay: 0.3, ease: "easeInOut" }}
+          >
+            <h1 className={styles.ttl}>{t("intro.title")}</h1>
+            <p className={styles.subttl}>{t("intro.subtitle")}</p>
+          </motion.div>
         </div>
       </header>
     </div>
