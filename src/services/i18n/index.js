767c1d69a3b58768e7cc51610d88f10101a4d952
felipeFerreiraffl <felipado2x0@gmail.com>
2025-08-06 19:16:56 -0300

    feat: configuration of i18n file

---
+++ src/services/i18n/index.js
@@ -0,0 +1,23 @@
+/* ---------- Arquivo de configuração de tradução ---------- */
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import I18NextHttpBackend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(I18NextHttpBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "pt-BR",
+    supportedLngs: ["pt-BR", "en"],
+    ns: ["translation"],
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+    },
+    interpolation: {
+      escapeValue: false, // React já faz a proteção contra XSS
+    },
+  });
+
+export default i18n;
