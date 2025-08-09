7692db23d8704069db00cc60769b6f644a0e7efe
felipeFerreiraffl <felipado2x0@gmail.com>
2025-08-09 00:02:43 -0300

    feat: color theme dropdown styled

--- src/components/Header/Dropdown/Color/index.jsx
+++ src/components/Header/Dropdown/Color/index.jsx
@@ -1,5 +1,10 @@
 import styles from "./styles.module.css";
 
 export default function ColorDropdown() {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <button className={`${styles.circle} ${styles.light}`}></button>
+      <button className={`${styles.circle} ${styles.dark}`}></button>
+    </div>
+  );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header";
+import ColorDropdown from "../../components/Header/Dropdown/Color";
 import styles from "./styles.module.css";
 
 export default function Home() {
   return (
     <div className={styles.home}>
       <Header />
+      <ColorDropdown />
     </div>
   );
 }
