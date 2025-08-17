fcbef7cb6861d2e56e2b36e4a0193891aac6bf0f
felipeFerreiraffl <felipado2x0@gmail.com>
2025-08-17 01:05:33 -0300

    style: added styles to image in intro section

--- src/components/Sections/Intro/index.jsx
+++ src/components/Sections/Intro/index.jsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.css";
 import { useTranslation } from "react-i18next";
 import SectionTitle from "../../SectionTitle";
+import images from "../../../services/utils/jsons/images";
 
 export default function Intro() {
   const { t } = useTranslation("sections");
@@ -10,16 +11,16 @@ export default function Intro() {
       <SectionTitle title={t("intro.title")} subtitle={t("intro.subtitle")} />
 
       <div className={styles.content}>
-        <div className={styles}>
-          <div className={styles}></div>
-          <div className={styles}></div>
-          <img src={""} alt="" />
+        <div className={styles.profileContainer}>
+          <div className={`${styles.ring} ${styles.firstRing}`}></div>
+          <div className={`${styles.ring} ${styles.secondRing}`}></div>
+          <img src={images.profile} alt="" loading="lazy" />
         </div>
 
         <div className={styles}>
           <div className={styles}>
-            <h2></h2>
-            <p></p>
+            <h2>Felipe Ferreira Lima</h2>
+            <p>{t("intro.infos.label")}</p>
           </div>
           <div className={styles}></div>
           <a href=""></a>
--- src/services/utils/jsons/images.js
+++ src/services/utils/jsons/images.js
@@ -1,11 +1,12 @@
 /* ---------- Objeto de imagens ---------- */
 import statue from "../../../assets/imgs/gris-staute.webp";
 import logo from "../../../assets/svgs/logos/logo.svg";
+import profile from "../../../assets/imgs/profile.webp";
 
 const images = {
   statue: statue,
   logo: logo,
-  profile: "",
+  profile: profile,
 };
 
 export default images;
