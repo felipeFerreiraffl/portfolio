3ce2e36b2297dfbd3d59c9f3c97b28b6209f5cbd
felipeFerreiraffl <felipado2x0@gmail.com>
2025-08-20 19:05:07 -0300

    feat: new animation on contacts links

--- src/components/Sections/Contacts/SocialIcon/index.jsx
+++ src/components/Sections/Contacts/SocialIcon/index.jsx
@@ -1,17 +1,50 @@
+import { useRef } from "react";
 import styles from "./styles.module.css";
+import { useGSAP } from "@gsap/react";
+import {
+  useGSAPConfigs,
+  useGSAPTimeline,
+} from "../../../../services/utils/hooks/global/gsap";
+import gsap from "gsap";
 
 export default function SocialIcon({ icon, title, href }) {
+  const ringRefs = useRef([]);
+  const tl = useRef();
+
+  useGSAPTimeline(
+    tl,
+    ringRefs,
+    { opacity: 0, scale: 0 },
+    {
+      opacity: 1,
+      scale: 1,
+      duration: 0.3,
+      ease: "power1.inOut",
+    },
+    0.15
+  );
+
+  const tlPlay = () => tl.current.play();
+  const tlReverse = () => tl.current.reverse();
+
   return (
     <a
       className={styles.link}
       href={href}
       target="_blank"
       rel="noopener noreferer"
+      onMouseEnter={tlPlay}
+      onMouseLeave={tlReverse}
       title={title}
       aria-label={title}
     >
-      <div className={`${styles.ring} ${styles.ringOne}`}></div>
-      <div className={`${styles.ring} ${styles.ringTwo}`}></div>
+      {[...Array(2)].map((_, i) => (
+        <div
+          key={i}
+          className={`${styles.ring} ${styles[`ring${i + 1}`]}`}
+          ref={(el) => (ringRefs.current[i] = el)}
+        ></div>
+      ))}
       <span>{icon}</span>
     </a>
   );
---
+++ src/services/utils/hooks/global/gsap.js
@@ -0,0 +1,30 @@
+/* ---------- Funções - GSAP ---------- */
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+export function useGSAPConfigs(ref, setProps = {}, toProps = {}) {
+  if (ref.current) {
+    gsap.set(ref.current, { ...setProps });
+    gsap.to(ref.current, { ...toProps });
+  }
+}
+
+export function useGSAPTimeline(
+  timeline,
+  refs = [],
+  fromProps = {},
+  toProps = {},
+  delay
+) {
+  useGSAP(
+    () => {
+      timeline.current = gsap.timeline({ paused: true });
+
+      refs.current.forEach((ring, i) => {
+        timeline.current.fromTo(ring, { ...fromProps }, { ...toProps }, i * delay);
+      });
+    },
+    { scope: refs }
+  );
+}
