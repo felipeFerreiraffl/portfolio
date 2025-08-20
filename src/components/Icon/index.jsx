963148f5b34ed6703eca7e4c6adde4862b0aad87
felipeFerreiraffl <felipado2x0@gmail.com>
2025-08-20 14:03:02 -0300

    chore: deleted local icons component

--- src/components/Icon/index.jsx
+++
@@ -1,6 +0,0 @@
-/* ---------- Componente de ícones ---------- */
-import styles from "./styles.module.css";
-
-export default function LocalIcon({ icon, alt }) {
-  return <img src={icon} alt={alt} className={styles.img} />;
-}
