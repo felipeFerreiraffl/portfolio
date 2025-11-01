54beb1de41fda9410f30de3f4dcb20dc2439382b
felipeFerreiraffl <felipado2x0@gmail.com>
2025-11-01 20:00:09 -0300

    feat: created first structure for maintenance page in html

--- src/pages/Maintainance/index.jsx
+++ src/pages/Maintainance/index.jsx
@@ -1,9 +1,23 @@
+import { useTranslation } from "react-i18next";
+import images from "../../services/utils/jsons/images";
 import styles from "./styles.module.css";
 
 export default function Maintenance() {
+  const { t } = useTranslation("common");
+
   return (
     <div>
-      <h1 className={styles}>Maintance!!!</h1>
+      <div className={styles}>
+        {[...Array(10)].map((_, i) => (
+          <div key={i} className={`${styles} ${styles}`}></div>
+        ))}
+      </div>
+      <div className={styles}>
+        <h1 className={styles}>{t("maintenance.title")} :(</h1>
+        <p className={styles}>{t("maintenance.subtitle")}</p>
+      </div>
+
+      <img src={images.logo} alt={"Logo - Felipe Ferreira Lima"} />
     </div>
   );
 }
