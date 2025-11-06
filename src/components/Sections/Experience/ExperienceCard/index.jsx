216a449c99fe82b5af7febf6d634284f1c11b1f6
felipeFerreiraffl <felipado2x0@gmail.com>
2025-11-05 21:20:19 -0300

    refactor: changed layout of experiences section - changed ExperienceCard - added data map

--- src/components/Sections/Experience/ExperienceCard/index.jsx
+++ src/components/Sections/Experience/ExperienceCard/index.jsx
@@ -1,19 +1,28 @@
+import { useTranslation } from "react-i18next";
 import styles from "./styles.module.css";
 
-export default function ExperienceCard({ src, title, time, desc }) {
+export default function ExperienceCard({ title, src, time, descs = [] }) {
+  const { t } = useTranslation("sections");
+
   return (
     <div className={styles.container} tabIndex={0}>
-      <div className={styles.img}>
-        <img src={src} alt={title} loading="lazy" title={title} />
+      <div className={styles.titleContainer}>
+        <h3 className={styles.title}>{t(title)}</h3>
+        <p className={styles.time}>{time}</p>
       </div>
 
-      <div className={styles.textContainer}>
-        <div className={styles.titleContainer}>
-          <h3>{title}</h3>
-          <p>{time}</p>
-        </div>
-        <p>{desc}</p>
-      </div>
+      <ul className={styles.list}>
+        {descs.map((desc, i) => (
+          <div className={styles.itemContainer}>
+            <span className={styles.marker}></span>
+            <li className={styles.item} key={i}>
+              {t(desc[`desc${i + 1}`])}
+            </li>
+          </div>
+        ))}
+      </ul>
+
+      <img className={styles.img} src={src} alt={title} />
     </div>
   );
 }
--- src/components/Sections/Experience/index.jsx
+++ src/components/Sections/Experience/index.jsx
@@ -1,11 +1,12 @@
 import { useTranslation } from "react-i18next";
-import { experiencesData } from "../../../data/data";
 import SectionTitle from "../../SectionTitle";
-import ExperienceCard from "./ExperienceCard";
 import styles from "./styles.module.css";
+import ExperienceCard from "./ExperienceCard";
+import images from "../../../utils/images";
+import { experiencesData } from "../../../data/data";
 
 export default function Experience({ ref }) {
-  const { t: tSections } = useTranslation("sections");
+  const { t } = useTranslation("sections");
 
   return (
     <div
@@ -14,43 +15,19 @@ export default function Experience({ ref }) {
       tabIndex={0}
     >
       <SectionTitle
-        title={tSections("experience.title")}
-        subtitle={tSections("experience.subtitle")}
+        title={t("experience.title")}
+        subtitle={t("experience.subtitle")}
       />
 
-      {experiencesData.map((exp, i) => (
+      {experiencesData.map((data, i) => (
         <ExperienceCard
           key={i}
-          title={tSections(exp.title)}
-          time={exp.time}
-          desc={tSections(exp.desc)}
-          src={exp.imgSrc}
+          title={data.title}
+          time={data.date}
+          src={data.src}
+          descs={data.descs}
         />
       ))}
-
-      <div className={styles.background}>
-        <div className={`${styles.treeContainer} ${styles.treeContainerLeft}`}>
-          <div className={styles.tree}>
-            <div className={`${styles.rectangle} ${styles.big}`}></div>
-            <div className={`${styles.stem} ${styles.stemBig}`}></div>
-          </div>
-          <div className={styles.tree}>
-            <div className={`${styles.rectangle} ${styles.small}`}></div>
-            <div className={`${styles.stem} ${styles.stemSmall}`}></div>
-          </div>
-        </div>
-
-        <div className={`${styles.treeContainer} ${styles.treeContainerRight}`}>
-          <div className={styles.tree}>
-            <div className={`${styles.rectangle} ${styles.small}`}></div>
-            <div className={`${styles.stem} ${styles.stemSmall}`}></div>
-          </div>
-          <div className={styles.tree}>
-            <div className={`${styles.rectangle} ${styles.big}`}></div>
-            <div className={`${styles.stem} ${styles.stemBig}`}></div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
