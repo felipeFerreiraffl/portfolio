66db60bb1492196fbf263bafabca5a9398944cd3
felipeFerreiraffl <felipado2x0@gmail.com>
2025-11-06 14:21:48 -0300

    style: added styles for skill component in skills section - added size, background-color, font, etc. - added rings without animation

--- src/components/Sections/Skills/Skill/index.jsx
+++ src/components/Sections/Skills/Skill/index.jsx
@@ -1,10 +1,19 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
+import { useGSAPTimeline } from "../../../../utils/hooks/global/gsap";
 import styles from "./styles.module.css";
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Skill({ name, icon }) {
   return (
-    <div className={styles}>
-      <h3 className={styles}>{name}</h3>
-      <span className={styles}>{icon}</span>
+    <div className={styles.container}>
+      {[...Array(2)].map((_, i) => (
+        <div key={i} className={styles.ring}></div>
+      ))}
+
+      <h3 className={styles.name}>{name}</h3>
+      <span className={styles.icon}>{icon}</span>
     </div>
   );
 }
--- src/components/Sections/Skills/index.jsx
+++ src/components/Sections/Skills/index.jsx
@@ -1,9 +1,7 @@
 import { useTranslation } from "react-i18next";
-import SectionTitle from "../../SectionTitle";
-import styles from "./styles.module.css";
-import Skill from "./Skill";
-import icons from "../../../utils/icons";
 import { ReactComponent as IconExemple } from "../../../assets/svgs/icons/typescript.svg";
+import SectionTitle from "../../SectionTitle";
+import Skill from "./Skill";
 
 export default function Skills({ ref }) {
   const { t: tSections } = useTranslation("sections");
@@ -15,7 +13,10 @@ export default function Skills({ ref }) {
         subtitle={tSections("skills.subtitle")}
       />
 
-      <Skill name={"Nome"} icon={<IconExemple fill="rgb(--color-text)" />} />
+      <Skill
+        name={"Nome"}
+        icon={<IconExemple />}
+      />
     </div>
   );
 }
