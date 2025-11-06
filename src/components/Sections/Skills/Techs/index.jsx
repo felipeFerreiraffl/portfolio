0d1d155154d1caa8d36393aaa4aca7da470bcf99
felipeFerreiraffl <felipado2x0@gmail.com>
2025-11-06 15:14:31 -0300

    style: added main styles in the techs components in skills component - added title and background illustration - imported skill component in techs

--- src/components/Sections/Skills/Techs/index.jsx
+++ src/components/Sections/Skills/Techs/index.jsx
@@ -1,9 +1,28 @@
+import Skill from "../Skill";
 import styles from "./styles.module.css";
+import { ReactComponent as IconEx } from "../../../../assets/svgs/icons/typescript.svg";
 
-export default function Techs({ title, techs = [] }) {
- return (
-   <div>
+export default function Techs({ title, techs = [], alignSelf }) {
+  return (
+    <div className={styles.container} style={{ "--align-self": alignSelf }}>
+      <div className={styles.mainContent}>
+        <div className={styles.symbolContainer}>
+          <span className={styles.diamond}></span>
+          <span className={styles.circle}></span>
+        </div>
 
-   </div>
- );
+        <h3 className={styles.title}>Título</h3>
+
+        <div className={styles.skillsContainer}>
+          <Skill name={"Habilidade 1"} icon={<IconEx />} />
+          <Skill name={"Habilidade 1"} icon={<IconEx />} />
+          <Skill name={"Habilidade 1"} icon={<IconEx />} />
+          <Skill name={"Habilidade 1"} icon={<IconEx />} />
+          <Skill name={"Habilidade 1"} icon={<IconEx />} />
+          <Skill name={"Habilidade 1"} icon={<IconEx />} />
+          <Skill name={"Habilidade 1"} icon={<IconEx />} />
+        </div>
+      </div>
+    </div>
+  );
 }
