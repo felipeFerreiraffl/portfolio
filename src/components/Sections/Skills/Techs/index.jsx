9d44472322396d46c5ea3d63986bb7c03e48260d
felipeFerreiraffl <felipado2x0@gmail.com>
2025-11-06 16:04:40 -0300

    feat: new animation on diamond background in techs component in skills section - added a scrollTrigger to rotate the element with scrub

--- src/components/Sections/Skills/Techs/index.jsx
+++ src/components/Sections/Skills/Techs/index.jsx
@@ -1,13 +1,39 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
+import { ReactComponent as IconEx } from "../../../../assets/svgs/icons/typescript.svg";
+import { useGSAPFromTo } from "../../../../utils/hooks/global/gsap";
 import Skill from "../Skill";
 import styles from "./styles.module.css";
-import { ReactComponent as IconEx } from "../../../../assets/svgs/icons/typescript.svg";
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Techs({ title, techs = [], alignSelf }) {
+  const diamondRef = useRef(null);
+
+  useGSAP(
+    () =>
+      useGSAPFromTo(
+        diamondRef,
+        { rotation: 0 },
+        {
+          rotation: 180,
+          duration: 0.6,
+          ease: "power1.inOut",
+          scrollTrigger: {
+            trigger: diamondRef.current,
+            scrub: 1,
+          },
+        }
+      ),
+    { scope: diamondRef }
+  );
+
   return (
     <div className={styles.container} style={{ "--align-self": alignSelf }}>
       <div className={styles.mainContent}>
         <div className={styles.symbolContainer}>
-          <span className={styles.diamond}></span>
+          <span ref={diamondRef} className={styles.diamond}></span>
           <span className={styles.circle}></span>
         </div>
 
