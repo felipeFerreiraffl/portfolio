7fb7675eda77dc75f12427f00adc5032989504c3
felipeFerreiraffl <felipado2x0@gmail.com>
2025-11-07 16:28:25 -0300

    style: added component into projects section - added ProjectCard into mapping on data - added container to them

--- src/components/Sections/Projects/index.jsx
+++ src/components/Sections/Projects/index.jsx
@@ -1,18 +1,30 @@
 import { useTranslation } from "react-i18next";
 import SectionTitle from "../../SectionTitle";
-import { projectsData } from "../../../data/data";
 import ProjectCard from "./ProjectCard";
+import { projectsData } from "../../../data/data";
 import styles from "./styles.module.css";
 
 export default function Projects({ ref }) {
-  const { t: tSections } = useTranslation("sections");
+  const { t } = useTranslation("sections");
 
   return (
     <div ref={ref} className="section-container" tabIndex={0}>
       <SectionTitle
-        title={tSections("projects.title")}
-        subtitle={tSections("projects.subtitle")}
+        title={t("projects.title")}
+        subtitle={t("projects.subtitle")}
       />
+
+      <div className={styles.cardsContainer}>
+        {projectsData.map((data, i) => (
+          <ProjectCard
+            key={i}
+            title={t(data.title)}
+            src={data.src}
+            desc={t(data.description)}
+            techs={data.techs}
+          />
+        ))}
+      </div>
     </div>
   );
 }
