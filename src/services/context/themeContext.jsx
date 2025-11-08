392f07f0895152b05d3316ddf5eec2d828e6066a
felipeFerreiraffl <felipado2x0@gmail.com>
2025-11-08 01:21:28 -0300

    feat(ThemeContext): added a color preference in theme context file - added a verification of color preference to set initial color theme

--- src/services/context/themeContext.jsx
+++ src/services/context/themeContext.jsx
@@ -5,17 +5,23 @@ import { createContext, useEffect, useState } from "react";
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState("light");
-
-  // Salva o tema no localStorage e aplica no documento
-  useEffect(() => {
+  const [theme, setTheme] = useState(() => {
     const savedTheme = localStorage.getItem("theme");
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
 
-    // Aplica o tema salvo ou o tema padrão (light)
     if (savedTheme) {
-      setTheme(savedTheme);
-      document.documentElement.setAttribute("data-theme", savedTheme);
+      return savedTheme;
     }
+
+    // Inicia o estado baseado no tema preferido ou padrão
+    return prefersDark ? "dark" : "light";
+  });
+
+  // Atualiza para o tema preferido ou padrão
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
   }, []);
 
   // Atualiza o tema no localStorage e no documento quando o tema muda
@@ -24,14 +30,36 @@ export function ThemeProvider({ children }) {
     document.documentElement.setAttribute("data-theme", theme);
   }, [theme]);
 
+  // Muda o tema quando a preferência do usuário muda
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const handleChange = (e) => {
+      const savedTheme = localStorage.getItem("theme");
+      if (!savedTheme) {
+        setTheme(e.matches ? "dark" : "light");
+      }
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+
+    return mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
   // Alterna entre os temas claro e escuro
   const setColorTheme = (theme) => {
-    setTheme(theme === "light" ? "light" : "dark");
+    setTheme(theme === "dark" ? "dark" : "light");
   };
+
+  console.log(
+    `Tema preferido: ${
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    }`
+  );
 
   return (
     <ThemeContext.Provider value={{ theme, setColorTheme }}>
-        {children}
+      {children}
     </ThemeContext.Provider>
-  )
+  );
 }
