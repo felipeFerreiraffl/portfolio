6fd1c6a9df8d79b71b5f499d192a71e3b0fbe9b9
felipeFerreiraffl <felipado2x0@gmail.com>
2025-11-07 23:24:07 -0300

    feat: added a new html structure for footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,109 @@
 import { useTranslation } from "react-i18next";
 import styles from "./styles.module.css";
+import { scrollToSection } from "../../utils/hooks/global/scrollToSection";
+import images from "../../utils/images";
 
-export default function Footer() {
+export default function Footer({ sectionRefs }) {
   const { t } = useTranslation("common");
 
   return (
     <footer className={styles.footer}>
-      <h2>{t("footer")}</h2>
-      <p>Felipe Ferreira Lima &copy; Copyright 2025</p>
+      <div className={styles}>
+        <div className={styles}>
+          <h2 className={styles}>{t("footer")}</h2>
+
+          <nav className={styles}>
+            <ul>
+              <li>
+                <a
+                  href="#"
+                  className={styles}
+                  title={"Intro"}
+                  aria-label={"Intro"}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    scrollToSection(sectionRefs.intro);
+                  }}
+                >
+                  Intro
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className={styles}
+                  title={t("navbar.experiences")}
+                  aria-label={t("navbar.experiences")}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    scrollToSection(sectionRefs.experiences);
+                  }}
+                >
+                  {t("navbar.experiences")}
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className={styles}
+                  title={t("navbar.skills")}
+                  aria-label={t("navbar.skills")}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    scrollToSection(sectionRefs.skills);
+                  }}
+                >
+                  {t("navbar.skills")}
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className={styles}
+                  title={t("navbar.projects")}
+                  aria-label={t("navbar.projects")}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    scrollToSection(sectionRefs.projects);
+                  }}
+                >
+                  {t("navbar.projects")}
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className={styles}
+                  title={t("navbar.contacts")}
+                  aria-label={t("navbar.contacts")}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    scrollToSection(sectionRefs.contacts);
+                  }}
+                >
+                  {t("navbar.contacts")}
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <span className={styles}>Copyright &copy; 2025</span>
+        </div>
+      </div>
+
+      <div className={styles}>
+        <div className={styles}>
+          <div className={styles}></div>
+        </div>
+
+        <img
+          src={images.statue}
+          alt={t("alts.statue")}
+          className={styles}
+          title={t("alts.statue")}
+          aria-label={t("alts.statue")}
+          loading="lazy"
+        />
+      </div>
     </footer>
   );
 }
